c1c3c68ba5d1bbac27cb314f92aa56d3b4bb86c8
sohailexe <div.sohailfaiz@gmail.com>
2024-12-12 21:02:29 +0500

    setup convex with clerk

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,10 +1,8 @@
-
-
 export default {
-    providers: [
-      {
-        domain: "https://eminent-worm-11.clerk.accounts.dev",
-        applicationID: "convex",
-      },
-    ]
-  };
+  providers: [
+    {
+      domain: "https://eminent-worm-11.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
